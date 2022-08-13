84172f9172e5c6e4e7c591cbdcb1a8a78fb4e7fa
Hyago Oliveira <hyagogow@gmail.com>
2022-08-13 15:16:46 -0300

    Add CryptographerFactory

---
+++ Runtime/CryptographerFactory.cs
@@ -0,0 +1,21 @@
+namespace ActionCode.Persistence
+{
+    public static class CryptographerFactory
+    {
+        /// <summary>
+        /// Creates an instance of ICryptographer based on the given type.
+        /// </summary>
+        /// <param name="type">The Cryptographer type to use.</param>
+        /// <param name="key">The key to use with the Cryptographer.</param>
+        /// <returns>An implementation of ICryptographer.</returns>
+        public static ICryptographer Create(CryptographerType type, string key)
+        {
+            return type switch
+            {
+                CryptographerType.None => null,
+                CryptographerType.AES => new AESCryptographer(key),
+                _ => null,
+            };
+        }
+    }
+}
